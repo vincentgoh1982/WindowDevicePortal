03b5c4b09a6db6c0158950a250f043a0867621f8
VG <goh.zhengyang.vincent@stengg.com>
2023-02-15 15:56:15 +0800

    Added new a class for Getting the list of map using the rest api and the main call the function

---
+++ WindowsDevicePortalWrapper/WindowsDevicePortalWrapper.Shared/HoloLens/HolographicMap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Microsoft.Tools.WindowsDevicePortal
+{
+    ///Add by ST Engineer
+    /// <content>
+    /// Wrappers for Mixed reality capture methods
+    /// </content>
+    public partial class DevicePortal
+    {
+
+        /// <summary>
+        ///  API for getting the list of Holographic Mixed Reality Map files.
+        /// </summary>
+        public static readonly string MrcMapListApi = "api/holographic/mapmanager/mapFiles";
+
+        /// <summary>
+        /// Gets the list of capture files
+        /// </summary>
+        /// <returns>List of the capture files</returns>
+        /// <remarks>This method is only supported on HoloLens.</remarks>
+        public async Task<string> GetMrcMapListAsync()
+        {
+
+            Uri uri = Utilities.BuildEndpoint(
+                this.deviceConnection.Connection,
+                MrcMapListApi);
+
+            Stream stream = await this.GetAsync(uri);
+            var sr = new StreamReader(stream);
+            string content = sr.ReadToEnd();
+
+            return content;
+        }
+    }
+}
